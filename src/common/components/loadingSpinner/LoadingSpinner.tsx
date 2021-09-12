e91db6fda3952b79c84572e838d3465441dd47b8
Ananda Mohanram <anandmohanram@gmail.com>
2021-09-13 04:02:23 +1200

    Create LoadingSpinner.tsx

---
+++ src/common/components/loadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+/**
+ * Loading spinner - a blue circular loading spinner
+ *
+ * @return {React.ReactElement<any, any>}
+ */
+export const LoadingSpinner = (): React.ReactElement<any, any> => {
+  return (
+    <div className="block">
+      <div
+        style={{ borderTopColor: "transparent" }}
+        className="m-auto w-16 h-16 border-8 border-blue-400 rounded-full animate-spin"
+      ></div>
+    </div>
+  );
+};
